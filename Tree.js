2acc2821afe5c16721597a2701438bd44c0cb60a
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-05-19 11:47:37 -0300

    Create sort and remove duplicate functions

---
+++ Tree.js
@@ -0,0 +1,46 @@
+import Node from "./Node.js";
+
+export default class Tree {
+  constructor(array) {
+    this.array = array;
+  }
+
+  removeDuplicates(array = this.array) {
+    const arrayToSet = new Set(array);
+
+    return [...arrayToSet];
+  }
+
+  sortArray(array = this.array) {
+    if(array.length === 1) return array;
+
+    let mid = Math.floor((array.length - 1) / 2);
+    let leftArrSorted = this.sortArray(array.slice(0, mid + 1));
+    let rightArrSorted = this.sortArray(array.slice(mid + 1, array.length));
+    let sortedArray = [];
+
+    // Merge sorted arrays
+    let l = 0;
+    let r = 0;
+    while((l < leftArrSorted.length) && (r < rightArrSorted.length)) {
+      const leftArrElement = leftArrSorted[l];
+      const rightArrElement = rightArrSorted[r];
+
+      if(leftArrElement < rightArrElement) {
+        sortedArray.push(leftArrElement);
+        l++;
+      } else {
+        sortedArray.push(rightArrElement);
+        r++;
+      }
+    }
+
+    sortedArray = sortedArray.concat(leftArrSorted.slice(l)).concat(rightArrSorted.slice(r));
+
+    return this.removeDuplicates(sortedArray);
+  }
+
+  buildTree(array = this.array) {
+    
+  }
+}
---
+++ main.js
@@ -0,0 +1,6 @@
+import Node from "./Node.js";
+import Tree from "./Tree.js";
+
+let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+console.log(tree.sortArray());
